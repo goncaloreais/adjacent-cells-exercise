d4bad8b2862cd64ed0d03df4d0b4d5bb0f7e8827
Gonçalo Reais <1170303@isep.ipp.pt>
2018-06-07 22:57:39 +0100

    exercise file and test files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# adjacent-cells-exercise
+# Adjacent Cells Exercise
+## Mindera Graduate Program
+
+This repository was created for the exercise of adjacent cells developed under the Mindera Graduate Program. It contains a javascript file (exercise.js) with all the code required to run the exercise and it also contains two json dummy files with grids provided by the exercise statement.
+
+###What is does?
+This exercise needs to be provided a grid (from one of the two dummy files) with 0s and 1s and its objetive is to find groups of 1s on the grid. It's important to mention that a group needs to have at least one 1, as said in the exercise statement.
+
+###Requirements:
+This project runs javascript and it has no special requirements such as libraries or frameworks. It just needs to run javascript, for instance using node.js on your local machine.
+
+###How to run:
+You can clone this project by running 
+```
+git clone https://github.com/goncaloreais/adjacent-cells-exercise.git
+```
+and then running the javascript file
+```
+node exercise.js
+```
+###Author
+This project was made by Gonçalo Reais.
+
+
+
---
+++ exercise.js
@@ -0,0 +1,85 @@
+var fs = require("fs");
+var gridExercise = fs.readFileSync("./exerciseGrid.json", "utf-8");
+var grid100x100 = fs.readFileSync("./100x100.json", "utf-8");
+
+//you can import your custom test file here
+//var [insertVariableName] = fs.readFileSync("./[insertFileName].json", "utf-8");
+
+//to run the program with your custom file, insert your variable name in parse function
+var grid = JSON.parse(gridExercise);
+
+var HEIGHT = grid.length;
+var WIDTH = grid[0].length;
+//GROUPS contains every group created
+var GROUPS = [];
+
+/**
+ * findInArray function checks if one cell is present in one array
+ * @param {*} array given array to search
+ * @param {*} point given point to search
+ */
+function findInArray(array, point) {
+    var pointToString = JSON.stringify(point);
+    var arrayToString = JSON.stringify(array);
+
+    if (arrayToString.indexOf(pointToString) != -1) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * findGroup is a recursive function that creates a group of 1s
+ * @param {*} group the group that is being built
+ * @param {*} i first coordinate
+ * @param {*} j second coordinate
+ */
+function findGroup(group, i, j) {
+    //exit condition
+    if (findInArray(group, [i, j])) {
+        return 0;
+    }
+
+    //push to group
+    group.push([i, j])
+
+    //check right
+    if (j + 1 < WIDTH && grid[i][j + 1] === 1) {
+        findGroup(group, i, j + 1);
+    }
+    //check left
+    if (j - 1 > 0 && grid[i][j - 1] === 1) {
+        findGroup(group, i, j - 1);
+    }
+    //check above
+    if (i + 1 < HEIGHT && grid[i + 1][j] === 1) {
+        findGroup(group, i + 1, j);
+    }
+    //check down
+    if (i - 1 > 0 && grid[i - 1][j] === 1) {
+        findGroup(group, i - 1, j);
+    }
+}
+
+//main loop
+//this loop checks every cell and create groups
+for (var i = 0; i < HEIGHT - 1; i++) {
+    for (var j = 0; j < WIDTH - 1; j++) {
+
+        var proceed = true;
+
+        if (grid[i][j] == 1) {
+            if (findInArray(GROUPS, [i, j])) {
+                proceed = false;
+            }
+
+            //if proceed is false, that means that the point is already on one group, so the program must not include it again
+            if (proceed) {
+                group = [];
+                findGroup(group, i, j);
+                console.log('Grupo: ' + JSON.stringify(group));
+                GROUPS.push(group);
+            }
+        }
+    }
+}
